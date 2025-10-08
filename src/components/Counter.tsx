41c716431fae91c9dc1c5e0d31e97590e9a862cb
realSlimPudge <komissarov010806@gmail.com>
2025-10-08 11:16:32 +0300

    initialCommit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import classes from "./Counter.module.scss";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={decrement} className={classes.btn}>
+        decrement
+      </button>
+      <span>{count}</span>
+      <button onClick={increment}>increment</button>
+    </div>
+  );
+};
